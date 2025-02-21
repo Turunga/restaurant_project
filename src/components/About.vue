d21d164b254f1499a86299b894ee70d040fd670c
Raphael Adams <mutungaadams01@gmail.com>
2025-02-21 12:13:53 +0300

    working on menu

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,69 @@
 <template>
-    <h1>about us</h1>
+ <v-container>
+
+        <v-card color="orange" >
+
+            <v-card-title>
+
+                <div class="text-h3">About us</div>
+
+            </v-card-title>
+
+        </v-card>
+
+    </v-container>
+    <v-card class="mx-auto" elevation="24">
+
+<v-carousel hide-delimiters>
+    <v-carousel-item src="/restaurant-interir.jpg" cover> </v-carousel-item>
+    <v-carousel-item src="/restaurant-interior.jpg" cover> </v-carousel-item>
+   
+</v-carousel>
+
+</v-card>
+<v-container>
+    <v-card class="mx-auto" elevation="36">
+        <v-container>
+        <v-row>
+            <v-col cols="100" md="24">
+                <v-card>
+                    <v-card color="white">
+                        <v-card-item>
+                            <v-card-title>About us</v-card-title>
+                        </v-card-item>
+                        <v-card-text>
+                            Welcome to Smokys, where passion, flavor, and hospitality come together to create an unforgettable dining experience. Nestled in the heart of Nairobi, our restaurant is more than just a place to eat—it’s a destination for those who appreciate high-quality cuisine, warm ambiance, and exceptional service
+                        </v-card-text>
+                    </v-card>
+                
+                    </v-card>
+
+                    </v-col>
+            </v-row>
+        </v-container>
+          </v-card>
+    </v-container>
+        <v-container>
+    <v-card class="mx-auto" elevation="36">
+        <v-container>
+        <v-row>
+            <v-col cols="100" md="24">
+                <v-card>
+                    <v-card color="white">
+                        <v-card-item>
+                            <v-card-title>Our story</v-card-title>
+                        </v-card-item>
+                        <v-card-text>
+                            At Smokys, we believe that great food brings people together. Our journey began with a simple vision: to create a space where guests can indulge in exquisite flavors, crafted with love and the finest ingredients. From humble beginnings, we have grown into a beloved establishment known for our commitment to culinary excellence and hospitality.
+                        </v-card-text>
+                    </v-card>
+                
+                    </v-card>
+
+                    </v-col>
+            </v-row>
+        </v-container>
+        </v-card>
+    </v-container>
+
 </template>
-
-<script setup>
-
-</script>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,8 +3,8 @@
 
 <v-carousel hide-delimiters>
     <v-carousel-item src="https://img.freepik.com/free-photo/shish-kebab-with-colorful-bell-peppers-served-with-ketchup-fresh-vegetables_141793-2935.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrid" cover> </v-carousel-item>
-    <v-carousel-item src="https://plus.unsplash.com/premium_photo-1670740967011-86730910a2e5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8fDA%3D" cover> </v-carousel-item>
-    <v-carousel-item src="https://images.unsplash.com/photo-1600891964599-f61ba0e24092?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8fDA%3D" cover> </v-carousel-item>
+    <v-carousel-item src="https://img.freepik.com/free-photo/chocolate-frappuccino-table_140725-8217.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrid" cover> </v-carousel-item>
+    <v-carousel-item src="https://img.freepik.com/free-photo/close-up-milkshakes-with-ice-cream_23-2148635651.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrid" cover> </v-carousel-item>
     <v-carousel-item src= "https://img.freepik.com/free-photo/asian-food-restaurant_7939-2017.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrids" cover> </v-carousel-item>
 
 </v-carousel>
@@ -20,7 +20,7 @@
                             <v-card-item>
                                 <v-card-title>New Menu</v-card-title>
 
-                                <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                                <v-card-subtitle>strawberry milkshake</v-card-subtitle>
                             </v-card-item>
                             <v-img height="200px" src="https://img.freepik.com/free-photo/strawberry-milkshake-with-whipped-cream-strawberries_140725-3182.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrid" cover ></v-img>
 
@@ -46,28 +46,31 @@
 
                 <v-col cols="12" md="4">
                     <v-card>
-                    <v-card color="yellow">
+                    <v-card color="black">
                     <v-card-item>
                     <v-card-title>Upcoming Events</v-card-title>
 
-                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    <v-card-subtitle>seasonal & holiday events</v-card-subtitle>
                     </v-card-item>
-                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover ></v-img>
+                    <v-img height="200px" src="https://img.freepik.com/free-photo/closeup-shot-barbequed-meat-glass-wine-near-fireplace_181624-58053.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrid" cover ></v-img>
+
 
                     <v-card-text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                        Get ready for a night of sizzling flavors, smoky goodness, and delicious BBQ favorites! Join us for mouthwatering grilled meats, tasty sides, and a great vibe. Don’t miss out—come hungry! on the 25th of february at 6:00pm
                     </v-card-text>
                     </v-card>
-                    <v-card color="yellow">
+                    <v-card color="black">
                     <v-card-item>
-                    <v-card-title>Upcoming Events</v-card-title>
+                    <v-card-title>New menu</v-card-title>
 
-                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    <v-card-subtitle>ultimate cheese burger</v-card-subtitle>
                     </v-card-item>
-                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover ></v-img>
+                    <v-img height="200px" src="https://img.freepik.com/free-photo/juicy-cheeseburger-rustic-wooden-board_9975-24623.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrid" cover ></v-img>
 
                     <v-card-text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                        Say hello to your new favorite burger! Juicy, flame-grilled beef, melted premium cheese, fresh toppings, and a soft toasted bun—pure perfection in every bite. Don’t just crave it, taste it!
+
+Available now. Come grab yours today! 
                     </v-card-text>
                     </v-card>
                 </v-card>
@@ -76,16 +79,18 @@
 
                 <v-col cols="12" md="4">
                     <v-card>
-                        <v-card color="yellow">
+                        <v-card color="orange">
                     <v-card-item>
                     <v-card-title>Promotions</v-card-title>
 
-                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    <v-card-subtitle>happy hour deals</v-card-subtitle>
                     </v-card-item>
-                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover ></v-img>
+                    <v-img height="200px" src="https://img.freepik.com/free-photo/grilled-chicken-meat-appetizer-spicy-with-honey-generative-ai_188544-12164.jpg?uid=R186033883&ga=GA1.1.712185049.1738746878&semt=ais_hybrid" cover ></v-img>
 
                     <v-card-text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                        Crispy, juicy, and packed with bold flavor—our Spicy Chicken Wings are the perfect Happy Hour snack! Enjoy them at 50% off during Happy Hour. Pair them with an ice-cold drink and make every bite count!
+
+ Available from [11:00-12:00] – Don’t Miss Out!
                     </v-card-text>
                         </v-card>
                         <v-card color="yellow">
@@ -107,8 +112,49 @@
         </v-container>
         </v-card>
     </v-container>
+    <v-container>
+        <v-card class="mx-auto" elevation="24" color = "purple">
+            <v-container>
+                <v-row >
+                    <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                                <v-card-title>Contact Us</v-card-title>
+                            </v-card-item>
+                            <v-card-text><v-icon icon="mdi-phone" /> 0722123456</v-card-text>
+                            <v-card-text><v-icon icon="mdi-email" /> myrestaurant@example.com</v-card-text>
+                            <v-card-text>Operating Hours: 10.00am - 10.00pm</v-card-text>
+                        </v-card>
+                    </v-col>
+
+                        <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                            <v-card-title>Find us on our socials</v-card-title>
+
+                            </v-card-item>
+                            <v-card-text><v-icon icon="mdi-instagram" /> myrestaurant</v-card-text>
+                            <v-card-text><v-icon icon="mdi-twitter" /> myrestaurant</v-card-text>
+                            <v-card-text><v-icon icon="mdi-facebook" /> myrestaurant</v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                            <v-card-title>Our Restaurant Locations</v-card-title>
+                            </v-card-item>
+                            <v-card-text>Madaraka</v-card-text>
+                            <v-card-text>CBD</v-card-text>
+                            <v-card-text>Langata</v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+    </v-container>
 </template>
 
 <script setup>
+
 
 </script>
